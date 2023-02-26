36a04bfdf09524e973ed16e36a813ade8df09a6b
Diyorbek-fronted <example@example.com>
2023-02-26 21:14:19 +0500

    init Commit

--- src/component/sign_up/signUp.js
+++ src/component/sign_up/signUp.js
@@ -6,10 +6,6 @@ const inactive = {}
 
 const SignUp = () => {
 
-    // const [selected, setSelect] = useState(0);
-    // const handleClick = () => {
-    //     setIsActive();
-    // };
     const [selected, setSelect] = useState(0);
     const prevClick = (num) => () => {
         setSelect(num);
@@ -17,17 +13,32 @@ const SignUp = () => {
 // eye add react 
     const [passwordType, setPasswordType] = useState("password");
     const [passwordInput, setPasswordInput] = useState("");
-    const handlePasswordChange =(evnt)=>{
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const handlePasswordChange = evnt =>{
         setPasswordInput(evnt.target.value);
     }
     const togglePassword =()=>{
-    if(passwordType==="password")
+    if(passwordType === "password")
     {
     setPasswordType("text")
     return;
     }
     setPasswordType("password")
     }
+
+    // Confirm
+    const handleConfirmChange = e => {
+        setConfirmPassword(e.target.value)
+    }
+    const toggleConfirm =()=>{
+        if(passwordType === "password")
+        {
+        setPasswordType("text")
+        return;
+        }
+        setPasswordType("password")
+        }
+
 
     return (
         <div className="sign_up">
@@ -40,45 +51,64 @@ const SignUp = () => {
                 <form action="/" className='forms'>
                     <label htmlFor="" className='label'>
                         <p className='form_text'>Email</p> 
-                        <div className = {`IcInput ${selected == 1 ? active : inactive}`} onClick ={prevClick(1)}  >
+                        <div className = {`IcInput ${selected === 1 ? active : inactive}`} onClick ={prevClick(1)}  >
                             <i class="fa-solid fa-envelope" ></i>
                             <input type="text" placeholder='Enter your email address' />
                         </div>                        
                     </label>
                     <label htmlFor="" className='label'>
                         <p className='form_text'>Username</p>
-                        <div className = {`IcInput ${selected == 2 ? active : inactive}`} onClick ={prevClick(2)}>
+                        <div className = {`IcInput ${selected === 2 ? active : inactive}`} onClick ={prevClick(2)}>
                             <i class="fa-solid fa-user i"></i>
                             <input type="text" placeholder='Username' />
                         </div>                        
                     </label>
                     <label htmlFor="">
                         <p className='form_text'>Password</p>
-                        <div className = {`IcInput ${selected == 3 ? active : inactive}`} onClick ={prevClick(3)} >
+                        <div className = {`IcInput ${selected === 3 ? active : inactive}`} onClick ={prevClick(3)} >
                             <i class="fa-solid fa-lock"></i>
-                            <input typtype={passwordType} onChange={handlePasswordChange} value={passwordInput} name="password" placeholder='Password' />
+                            <input type={passwordType} onChange={handlePasswordChange} value={passwordInput} name="password" placeholder='Password' />
                             <div className='eye_icons' onClick={togglePassword}>
-                                { passwordType == "password" ? <i class = "fa-solid fa-eye"></i> : <i class = "fa-solid fa-eye-slash"></i> }
+                                { passwordType === "password" ? <i class = "fa-solid fa-eye-slash"></i> : <i class = "fa-solid fa-eye"></i> }
                             </div>
                         </div>                        
                     </label>
                     <label htmlFor="/">
                         <p className='form_text'>Confirm Password</p>
-                        <div className = {`IcInput ${selected == 4 ? active : inactive}`} onClick ={prevClick(4)}>
+                        <div className = {`IcInput ${selected === 4 ? active : inactive}`} onClick ={prevClick(4)}>
                             <i class="fa-solid fa-lock"></i>
-                            <input type="password" placeholder='Confirm Password' />
-                            <span className='eye_icons' >
-                                <i class="fa-solid fa-eye"></i>
-                                <i class="fa-solid fa-eye-slash"></i>
-                            </span>
+                            <input  type={passwordType} onChange={handleConfirmChange} value={confirmPassword} name="Confirm password" placeholder='Confirm Password'  />
+                            <div className='eye_icons' onClick={toggleConfirm}>
+                                { passwordType === "password" ? <i class = "fa-solid fa-eye-slash"></i> : <i class = "fa-solid fa-eye"></i> }
+                            </div>
                         </div>                        
                     </label>
+                    <div className="buttom">
+                        <button type='submit'>Register</button>
+                    </div>
                 </form>
             </div>
             <div className="right_img_sign">
-                <div>
-
-                </div>
+                {/* <div className='box-right'>
+                    <span className='box_star'>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </span>
+                    <div className="box_text_right">
+                        <h1 className='text_h1_right'>
+                            ExpensiM 
+                            <span className='text_span_min'>TM</span>
+                        </h1>
+                    </div>
+                    <div className="last_text">
+                        <h4>Sign Up to name</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div> */}
+                <img src={require('./rasmm.png')} alt="" />
             </div>
         </div>
     )
